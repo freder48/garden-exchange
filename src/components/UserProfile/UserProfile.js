30a0969aab68b9eb93ee02a503956b261674a564
Josie Fredericksen <josiefredericksen@josies-mbp.attlocal.net>
2021-01-09 15:24:26 -0600

    Get user listings rendering to dom + material ui icons

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
+import Moment from 'react-moment';
+import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
+import EditIcon from '@material-ui/icons/Edit';
 
 class UserProfile extends Component {
     //get all messages for specific user upon page load
@@ -11,30 +14,39 @@ class UserProfile extends Component {
     render() {
         return (
             <div>
+                {JSON.stringify(this.props.store.forum)}
                 <h2>Profile</h2>
                 <h4>Your Listings</h4>
-                {/* <table>
+                <table>
                     <thead>
                         <tr>
-                            <th>Message</th>
-                            <th>Received At</th>
-                            <th>Reply</th>
+                            <th>Have</th>
+                            <th>Want</th>
+                            <th>Location</th>
+                            <th>Date</th>
+                            <th>Edit</th>
                             <th>Delete</th>
                         </tr>
                     </thead>
 
                     <tbody>
+                         {this.props.store.forum.map((listing) =>
 
-                        {this.props.store.message.map((message) =>
-
-                            <tr key={message.id}>
-
+                            <tr key={listing.id}>
+                                <td>{listing.have}</td>
+                                <td>{listing.want}</td>
+                                <td>{listing.location}</td>
+                                <td><Moment format='MM/DD/YYYY'>{listing.date}</Moment></td>
+                                <td><EditIcon></EditIcon></td>
+                                <td><DeleteOutlinedIcon
+                                    onClick={() => { this.deleteListing(listing.id) }}>
+                                </DeleteOutlinedIcon></td>
                             </tr>
 
-                        )}
+                        )} 
 
                     </tbody>
-                </table> */}
+                </table>
             </div>
         );
     }
