299c9bfef6ba8491cf6a7a111779ffbfd44da327
Josie Fredericksen <josiefredericksen@josies-mbp.attlocal.net>
2021-01-09 11:20:51 -0600

    functioning get route for user messages

--- server/routes/message.router.js
+++ server/routes/message.router.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
 
-router.get('/', (req, res) => {
-  // GET route code here
-});
-
+  // This route will return logged in users messages
+router.get('/', rejectUnauthenticated, (req, res) => {
+  let queryText = `SELECT * FROM "message" WHERE "sent_to_user_id" = $1;`;
+  pool.query(queryText, [req.user.id]).then((result) => {
+      res.send(result.rows);
+  }).catch((error) => {
+      console.log(error);
+      res.sendStatus(500);
+  });
+});//end GET
 
 //POST message
 router.post('/', (req, res) => {
