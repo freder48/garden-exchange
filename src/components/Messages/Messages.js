bcd67f8a369db0c6cdef87e5338d02b141366ec3
Josie Fredericksen <josiefredericksen@josies-mbp.attlocal.net>
2021-01-09 12:12:03 -0600

    imported reply and delete icons

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -3,6 +3,8 @@ import './Messages.css'
 import Moment from 'react-moment';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 import { connect } from 'react-redux';
+import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
+import ReplyIcon from '@material-ui/icons/Reply';
 
 class Messages extends Component {
 //get all messages for specific user upon page load
@@ -31,11 +33,16 @@ class Messages extends Component {
                     
                     <tr key={message.id}>
                         <td>{message.message}</td>
-                        <td><Moment format='hh:mm A, MM/DD/YYYY'>{message.time_received}</Moment></td>
+                        <td><Moment format='hh:mm A, MM/DD/YYYY'>
+                            {message.time_received}
+                            </Moment></td>
+                        <td><ReplyIcon></ReplyIcon></td>
+                        <td><DeleteOutlinedIcon 
+                            onClick={()=> {this.deleteMessage(message.id)}}>
+                            </DeleteOutlinedIcon></td>
                     </tr>
 
                     )}
-
                      
                     </tbody>
                 </table>
