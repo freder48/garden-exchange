176b769d661786152e656c2bd939b09efd459a48
Josie Fredericksen <josiefredericksen@josies-mbp.attlocal.net>
2021-01-08 19:06:06 -0600

    added textfields

--- src/components/AddListing/AddListing.js
+++ src/components/AddListing/AddListing.js
@@ -4,6 +4,10 @@ import { withStyles } from '@material-ui/core/styles';
 import { TextField, Button, Grid, Card, CardContent, Typography } from '@material-ui/core';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 
+const styles = {
+    
+}
+
 class AddListing extends Component {
     state = {
         newListing: {
@@ -24,6 +28,53 @@ class AddListing extends Component {
                             <Typography gutterBottom variant="h4" component="h2" className={classes.header}>
                                 Add Listing
                         </Typography>
+                            <form onSubmit={this.handleSubmit} className={classes.form}>
+
+                                <TextField
+
+                                    label="Have"
+                                    variant="filled"
+                                    required
+                                    className={classes.textField}
+                                    value={this.state.newListing.have}
+                                    onChange={(event) => this.handleChange('have', event)}
+                                />
+
+
+                                <br></br>
+                                <br></br>
+
+                                <TextField
+                                    required
+                                    label="Want"
+                                    variant="filled"
+                                    className={classes.textField}
+                                    value={this.state.newListing.want}
+                                    onChange={(event) => this.handleChange('want', event)}
+                                />
+
+                                <br></br>
+                                <br></br>
+                                <TextField
+                                    required
+                                    label="Location"
+                                    variant="filled"
+                                    className={classes.textField}
+                                    value={this.state.newListing.location}
+                                    onChange={(event) => this.handleChange('location', event)}
+                                />
+
+                                <br></br>
+                                <br></br>
+
+                                <Button
+                                    className={classes.button}
+                                    onClick={this.handleSubmit}
+                                    variant="outlined"
+                                >
+                                    Submit
+                                </Button>
+                            </form>
                         </CardContent>
                     </Card>
                 </Grid>
